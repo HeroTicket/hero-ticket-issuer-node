1cda6e512cd5073d62ddc1d32a09968db3b31fa5
Lee Hyohwak <crewe1746@naver.com>
2023-11-20 17:59:44 +0900

    Add config

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+import config from './config.json';
+
+export default {
+    RHS_URL: config.rhs_url,
+    CONTRACT_ADDRESS: config.contract_address,
+    CIRCUITS_PATH: config.circuits_path,
+    RPC_URL: config.rpc_url,
+    WALLET_KEY: config.wallet_key,
+};
