a1d7f94d59de72eaf0415cc0e34684245776c61a
Lee Hyohwak <crewe1746@naver.com>
2023-11-20 17:42:35 +0900

    Setup stub express server

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log('Hello World')
+import express, { Express, Request, Response } from 'express';
+
+const main = async () => {
+    const app: Express = express();
+
+    const port = 3001;
+
+    app.get('/', (req: Request, res: Response) => {
+        res.send('Hello World!');
+    });
+
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
+
+main();
